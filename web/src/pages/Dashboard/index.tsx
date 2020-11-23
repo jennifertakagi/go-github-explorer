3119190ba8152529283506f0ecf676756404966c
jennifertakagi <jennitakagi@gmail.com>
2020-11-23 18:52:58 -0300

    feat: add repository card and its styles

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logo} alt="Github Explorer logo" />
+      <Title>Explore repositories on GitHub</Title>
+
+      <Form>
+        <input placeholder="Search by repository name" />
+        <button type="submit">Search</button>
+      </Form>
+
+      <Repositories>
+        <a href="https://github.com/jennifertakagi">
+          <img
+            alt="Jennifer Takagi"
+            src="https://avatars2.githubusercontent.com/u/31541321?s=460&u=9ed794fbd85ae1e136fdd3910ec0eee70911a7cb&v=4"
+          />
+          <div>
+            <strong>sveltejs / svelte</strong>
+            <p>Cybernetically enhanced web apps</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
