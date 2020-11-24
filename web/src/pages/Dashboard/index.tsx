07ad1c38da1af8520b80a7c464bc8fa291c3231a
jennifertakagi <jennitakagi@gmail.com>
2020-11-24 10:40:10 -0300

    feat: get repositories from github api and show them

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,32 +1,66 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [newRepo, setNewRepo] = useState('');
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const { data: repository } = await api.get<Repository>(`repos/${newRepo}`);
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logo} alt="Github Explorer logo" />
       <Title>Explore repositories on GitHub</Title>
 
-      <Form>
-        <input placeholder="Search by repository name" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Search by repository name"
+        />
         <button type="submit">Search</button>
       </Form>
 
       <Repositories>
-        <a href="https://github.com/jennifertakagi">
-          <img
-            alt="Jennifer Takagi"
-            src="https://avatars2.githubusercontent.com/u/31541321?s=460&u=9ed794fbd85ae1e136fdd3910ec0eee70911a7cb&v=4"
-          />
-          <div>
-            <strong>sveltejs / svelte</strong>
-            <p>Cybernetically enhanced web apps</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repository => (
+          <a
+            key={repository.full_name}
+            href="https://github.com/jennifertakagi"
+          >
+            <img
+              alt={repository.owner.login}
+              src={repository.owner.avatar_url}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
