ab99f7d8b5d3ce6e0e366e60d14c7121efd0226e
jennifertakagi <jennitakagi@gmail.com>
2020-11-29 10:46:26 -0300

    feat: create Repository page and style

--- web/src/pages/Repository/index.tsx
+++ web/src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+
+import { Issues, Header, RepositoryInfo } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +13,49 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1> Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Back
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src={logo} alt="Github Explorer" />
+          <div>
+            <strong>bla</strong>
+            <p>description</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1809</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1809</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1809</strong>
+            <span>Open Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="/">
+          <div>
+            <strong>sasas</strong>
+            <p>ahiusa</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
